4f32acd38ebe2849be8a7b7bc3ee5c375d8ff1de
James Hunt <james@niftylogic.com>
2020-02-21 23:03:29 -0500

    Clean up vaultd impl

--- src/bin/vaultd.rs
+++ src/bin/vaultd.rs
@@ -17,6 +17,52 @@ fn boot() -> API {
     )
 }
 
+fn param(r: &Request, name: &str) -> Option<String> {
+    match r.extensions.get::<Router>() {
+        None => None,
+        Some(params) => match params.find(name) {
+            None => None,
+            Some(v) => Some(v.to_string()),
+        },
+    }
+}
+
+macro_rules! param {
+    ($r: expr, $e: expr) => {
+        match param($r, $e) {
+            Some(v) => v,
+            None => return bad_request!("bad request"),
+        }
+    };
+}
+
+macro_rules! bad_request {
+    ($s: expr) => {
+        Ok(Response::with((status::BadRequest, $s)))
+    };
+}
+
+macro_rules! okay {
+    ($o: expr) => {
+        Ok(Response::with((status::Ok, json!($o).to_string())))
+    };
+}
+
+macro_rules! forbidden {
+    ($s: expr) => {
+        Ok(Response::with((status::Forbidden, $s)))
+    };
+}
+
+macro_rules! unimpl {
+    ($s: expr) => {
+        Ok(Response::with((
+            status::InternalServerError,
+            format!("{}: unimplemented.", $s),
+        )))
+    };
+}
+
 fn main() {
     let mut router = Router::new();
 
@@ -27,13 +73,13 @@ fn main() {
             match serde_json::from_reader(&mut r.body) {
                 Err(e) => {
                     println!("error: {}", e);
-                    Ok(Response::with((status::BadRequest, "bad request")))
+                    bad_request!("bad request")
                 }
                 Ok(attempt) => match api.authenticate(attempt) {
-                    Ok(res) => Ok(Response::with((status::Ok, json!(res).to_string()))),
+                    Ok(res) => okay!(res),
                     Err(e) => {
                         println!("authn fail: {}", e);
-                        Ok(Response::with((status::Forbidden, "authn failed")))
+                        forbidden!("authentication failed")
                     }
                 },
             }
@@ -48,13 +94,13 @@ fn main() {
             match serde_json::from_reader(&mut r.body) {
                 Err(e) => {
                     println!("error: {}", e);
-                    Ok(Response::with((status::BadRequest, "bad request")))
+                    bad_request!("bad request")
                 }
                 Ok(attempt) => match api.signup(attempt) {
-                    Ok(res) => Ok(Response::with((status::Ok, json!(res).to_string()))),
+                    Ok(res) => okay!(res),
                     Err(e) => {
                         println!("authn fail: {}", e);
-                        Ok(Response::with((status::Forbidden, "authn failed")))
+                        forbidden!("authentication failed")
                     }
                 },
             }
@@ -64,10 +110,7 @@ fn main() {
 
     router.get(
         "/v1/collectors/:uid/decks",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        |_r: &mut Request| unimpl!("deck retrieval"),
         "v1_get_all_transactions_handler",
     );
 
@@ -75,17 +118,17 @@ fn main() {
         "/v1/collectors/:uid/decks",
         |r: &mut Request| {
             let api = boot();
-            let uid = r.extensions.get::<Router>().unwrap().find("uid").unwrap();
+            let uid = param!(r, "uid");
             match serde_json::from_reader(&mut r.body) {
                 Err(e) => {
                     println!("error: {}", e);
-                    Ok(Response::with((status::BadRequest, "bad request")))
+                    bad_request!("bad request")
                 }
-                Ok(attempt) => match api.post_transaction(uid, attempt) {
-                    Ok(res) => Ok(Response::with((status::Ok, json!(res).to_string()))),
+                Ok(attempt) => match api.post_transaction(&uid, attempt) {
+                    Ok(res) => okay!(res),
                     Err(e) => {
                         println!("transaction fail: {}", e);
-                        Ok(Response::with((status::Forbidden, "transaction failed")))
+                        forbidden!("transaction creation failed")
                     }
                 },
             }
@@ -94,38 +137,26 @@ fn main() {
     );
 
     router.get(
-        "/v1/collectors/:uid/decks/:tid",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        "/v1/collectors/:uid/transactions/:tid",
+        |_r: &mut Request| unimpl!("transaction retrieval"),
         "v1_get_single_transaction_handler",
     );
 
     router.patch(
-        "/v1/collectors/:uid/decks/:tid",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        "/v1/collectors/:uid/transactions/:tid",
+        |_r: &mut Request| unimpl!("transaction update"),
         "v1_update_single_transaction_handler",
     );
 
     router.delete(
-        "/v1/collectors/:uid/decks/:tid",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        "/v1/collectors/:uid/transactions/:tid",
+        |_r: &mut Request| unimpl!("transaction removal"),
         "v1_delete_single_transaction_handler",
     );
 
     router.get(
         "/v1/collectors/:uid/decks",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        |_r: &mut Request| unimpl!("deck retrieval"),
         "v1_get_all_decks_handler",
     );
 
@@ -133,17 +164,17 @@ fn main() {
         "/v1/collectors/:uid/decks",
         |r: &mut Request| {
             let api = boot();
-            let uid = r.extensions.get::<Router>().unwrap().find("uid").unwrap();
+            let uid = param!(r, "uid");
             match serde_json::from_reader(&mut r.body) {
                 Err(e) => {
                     println!("error: {}", e);
-                    Ok(Response::with((status::BadRequest, "bad request")))
+                    bad_request!("bad request")
                 }
-                Ok(attempt) => match api.post_deck(uid, attempt) {
-                    Ok(res) => Ok(Response::with((status::Ok, json!(res).to_string()))),
+                Ok(attempt) => match api.post_deck(&uid, attempt) {
+                    Ok(res) => okay!(res),
                     Err(e) => {
                         println!("deck fail: {}", e);
-                        Ok(Response::with((status::Forbidden, "deck failed")))
+                        forbidden!("deck creation failed")
                     }
                 },
             }
@@ -153,32 +184,26 @@ fn main() {
 
     router.get(
         "/v1/collectors/:uid/decks/:did",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        |_r: &mut Request| unimpl!("deck retrevial"),
         "v1_get_single_deck_handler",
     );
 
     router.patch(
         "/v1/collectors/:uid/decks/:tid",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        |_r: &mut Request| unimpl!("deck update"),
         "v1_update_single_deck_handler",
     );
 
     router.delete(
         "/v1/collectors/:uid/decks/:did",
-        |r: &mut Request| {
-            let api = boot();
-            Ok(Response::with((status::Ok, "hi")))
-        },
+        |_r: &mut Request| unimpl!("deck removal"),
         "v1_delete_single_deck_handler",
     );
 
-    let bind = "localhost:3000";
-    println!("example-web starting up on {}", bind);
+    let bind = match env::var("VCB_LISTEN") {
+        Ok(v) => v,
+        Err(_) => "localhost:3000".to_string(),
+    };
+    println!("vault-of-cardboard starting up on {}", bind);
     Iron::new(router).http(bind).unwrap();
 }
