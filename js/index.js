7d9e3b3ad4ad1b7a2a5f51c578b32214ddaff1b5
James Hunt <james@niftylogic.com>
2021-08-19 02:19:29 +0000

    Remove js/* (old legacy)

--- js/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = Object.freeze(Object.assign({},
-  require('./api.js'),
-  require('./cdif.js'),
-  require('./draft.js'),
-  require('./helpers.js'),
-  require('./query.js'),
-  require('./vault.js'),
-  require('./when.js')));
